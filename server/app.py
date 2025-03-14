6d2e30829d396e689535849ddb992d56bde762ac
Brandon Bakus <brandonbakus@gmail.com>
2025-03-13 20:47:51 -0400

    I like this more

--- server/app.py
+++ server/app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -13,15 +11,48 @@ app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
-
 api = Api(app)
 
+# For testing purposes
+with app.app_context():
+    db.create_all()
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plants_dict = [plant.to_dict() for plant in plants]
+        return plants_dict, 200
+    
+    def post(self):
+        # Get data from JSON if available, otherwise from form
+        if request.is_json:
+            data = request.get_json()
+        else:
+            data = request.form
+        
+        # Create new plant
+        new_plant = Plant(
+            name=data.get('name'),
+            image=data.get('image'),
+            price=data.get('price')
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        return new_plant.to_dict(), 201
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
         
+        if not plant:
+            return {"error": "Plant not found"}, 404
+            
+        return plant.to_dict(), 200
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from flask import Flask, request, make_response
 
 db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    image = db.Column(db.Text)
+    price = db.Column(db.Float)
+
+
+  
+
+
